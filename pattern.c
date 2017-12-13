e2a79ae65f2a26439d45492a45939587005f7d4d
Chanchal Saini <31559139+chanchalsaini@users.noreply.github.com>
2017-12-13 19:56:54 +0530

    Create pattern.c

---
+++ pattern.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main(){
+          printf("This program will [print the trianglew in decreasing order of the number givebn by you\n");
+          printf("Enter the integral number\n");
+          int n;
+          for(i=n;i>=1;i--){
+                  for(k=i;k>=1;k--){
+                  printf("%d",k);
+                  }
+                         for(j=2;j<=i;j++){
+                         printf("%d",j);
+                         }
+                         printf("\n");
+                               for(l=0;l=n-i;l++){
+                               printf(" ");
+                               }
+          }
+        }
+          
