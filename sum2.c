e2cd2ff54b989132364325841bab841b668a063a
Chanchal Saini <31559139+chanchalsaini@users.noreply.github.com>
2017-12-14 05:34:43 +0530

    Create sum2.c

---
+++ sum2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+	int a,b;
+	printf("This is the program to print sum of 2 numbers\n");
+	printf("Enter the first number\n");
+	scanf("%d",&a);
+	printf("Enter the second number\n");
+	scanf("%d",&b);
+	int c=a+b;
+	printf("Sum is %d\n",c);
+}
